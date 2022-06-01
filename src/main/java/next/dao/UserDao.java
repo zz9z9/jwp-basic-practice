adf9da99d127f3511556a7dfae75d73dfa462788
Jaeyoon <wodbs4338@naver.com>
2022-06-02 01:39:07 +0900

    7장 실습 - userDao 클래스 update, findAll 메서드 구현

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -36,12 +36,35 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
+
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+
+            pstmt.executeUpdate();
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        String sql = "SELECT * FROM USERS";
+        List<User> users = new ArrayList<>();
+
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                User user = new User(rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email"));
+
+                users.add(user);
+            }
+        }
+
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
